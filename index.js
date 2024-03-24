bc16da843dd066941bf5b7473c9cfba8e55c38a2
mwaikin <mwaikingori10@gmail.com>
2024-03-24 08:25:34 +0300

    complete for loop
    complete while loop

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function writeCards( names, gift ) {
+  return names.map( name => `Thank you, ${name}, for the wonderful ${gift} gift!` );
+}
+function countDown(number){
+  while (number >= 0){
+    console.log(number);
+    number--;
+}
+}
+countDown(number);
